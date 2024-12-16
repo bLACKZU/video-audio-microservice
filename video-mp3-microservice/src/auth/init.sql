40fe8127dd29d94b6c06bbb4c8aee8d79cfc58fe
bLACKZU <satyakighosh65@gmail.com>
2024-12-17 01:27:01 +0530

    auth service WIP

---
+++ video-mp3-microservice/src/auth/init.sql
@@ -0,0 +1,16 @@
+CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Auth123';
+
+CREATE DATABASE auth;
+
+GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
+
+USE auth;
+
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email varchar(50) NOT NULL,
+    password varchar(20) NOT NULL
+);
+
+
+INSERT INTO user(email, password) VALUES ('satyakighosh65@gmail.com', 'admin123')
